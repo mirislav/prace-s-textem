ddb880e787eb34dfcf5e972edbd39e93cd4f05ea
mirislav <128368249+mirislav@users.noreply.github.com>
2024-01-30 10:35:56 +0100

    Add files via upload

---
+++ procviceni/script.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    const strRev =  str.split('').reverse().join('');
+    console.log(strRev);
+}
+
+
+
+
+//adam kopiroval cely kod z chat.gpt a daval to do ai detectoru\\
